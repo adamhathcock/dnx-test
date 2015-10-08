5e65daef1c9c471467d513ddadbeb2b57a2d97b4
Adam Hathcock <adam.hathcock@autodesk.com>
2015-10-08 10:13:40 +0100

    Test complete

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Microsoft.Dnx.Runtime;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private readonly ILibraryManager libraryManager;
+
+        public Program(ILibraryManager libraryManager)
+        {
+            this.libraryManager = libraryManager;
+        }
+
+        public void Main(string[] args)
+        {
+            var c = new Class1();
+            c.DoIt();
+
+            var t = Type.GetType("Class1");
+            if (t != null)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+            var library = libraryManager.GetLibrary("ClassLibrary1");
+            var a = Assembly.Load(library.Assemblies.First());
+            t = a.GetType("Class1");
+            if (t != null)
+            {
+                Console.WriteLine(t.Name);
+            }
+            t = a.GetTypes().First();
+            Console.WriteLine(t.Name);
+        }
+    }
+}
